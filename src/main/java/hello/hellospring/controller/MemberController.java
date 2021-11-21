6db487f23b0ae55b04d3756054def3f75dd5f2bf
이원기 <wongi@iwongiui-MacBookPro.local>
2021-11-21 18:53:57 +0900

    Initial commit

---
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -0,0 +1,61 @@
+package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @Autowired
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+//        System.out.println("memberService = " + memberService.getClass());  // AOP 적용 시 프록시 패턴이 적용되었나 확인하는 코드
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
+
+
+
+//    @Autowired
+//    private MemberService memberService;  -> 의존성 필드 주입
+
+//    private MemberService memberService;
+//
+//    @Autowired
+//    public void setMemberService(MemberService memberService) {
+//        this.memberService = memberService;
+//    }  -> 의존성 Setter 주입
+
+
+
+}
